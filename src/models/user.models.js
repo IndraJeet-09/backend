143318a8e3a66bdf69e872e7410453e4f915305d
Indrajeet Singh <indrajeetchouhan680@gmail.com>
2025-05-29 17:50:20 +0530

    Project setup ends

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -47,10 +47,9 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
-
-    this.password = await bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
